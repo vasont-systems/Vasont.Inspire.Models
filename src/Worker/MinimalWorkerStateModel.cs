8b6ad06a690e2681cce88d0881baa457029a51f3
Rob Kennedy (Admin) <rkennedy@vasont.com>
2019-12-31 23:25:16 -0500

    added missing worker models

---
+++ src/Worker/MinimalWorkerStateModel.cs
@@ -0,0 +1,109 @@
+﻿//-------------------------------------------------------------
+// <copyright file="MinimalWorkerStateModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------
+namespace Vasont.Inspire.Models.Worker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using Vasont.Inspire.Core.Errors;
+    using Vasont.Inspire.Core.Properties;
+
+    /// <summary>
+    /// This class represents a minimal representation of a background worker state.
+    /// </summary>
+    public class MinimalWorkerStateModel
+    {
+        /// <summary>
+        /// Gets or sets the worker cache life in minutes.
+        /// </summary>
+        public int CacheLifeInMinutes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tenant domain key of the owning tenant.
+        /// </summary>
+        public string TenantDomainKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets a unique key for the worker process.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identity of the worker process.
+        /// </summary>
+        public Guid ProcessId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the default language for the worker process.
+        /// </summary>
+        public string DefaultLanguage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the worker state status.
+        /// </summary>
+        public WorkerStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the process was executed.
+        /// </summary>
+        public DateTime SubmittedUtcDate { get; set; }
+
+        /// <summary>
+        /// Gets a string of the length of time the process has been running.
+        /// </summary>
+        public string Runtime
+        {
+            get
+            {
+                TimeSpan span = DateTime.UtcNow - this.SubmittedUtcDate;
+                return string.Format(CultureInfo.InvariantCulture, Resources.RuntimeMessageText, span.Days, span.Hours, span.Minutes, span.Seconds);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a message value used for multi-purpose messaging for the process.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of issues from the import process.
+        /// </summary>
+        public List<ErrorMessage> Issues { get; set; } = new List<ErrorMessage>();
+    }
+
+    /// <summary>
+    /// This class represents worker state within the application that contains an additional 
+    /// model object.
+    /// </summary>
+    /// <typeparam name="T">Contains the object type of the additional worker model.</typeparam>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Class files would be named the same.")]
+    public sealed class MinimalWorkerStateModel<T> : MinimalWorkerStateModel
+    {
+        #region Public Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinimalWorkerStateModel{T}"/> class.
+        /// </summary>
+        public MinimalWorkerStateModel()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MinimalWorkerStateModel{T}"/> class.
+        /// </summary>
+        /// <param name="model">Contains any additional model used with the worker.</param>
+        public MinimalWorkerStateModel(T model)
+        {
+            this.Model = model;
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets or sets the optional model for the worker.
+        /// </summary>
+        public T Model { get; set; }
+    }
+}
---
+++ src/Worker/WorkerStateModel.cs
@@ -0,0 +1,145 @@
+﻿//-------------------------------------------------------------
+// <copyright file="WorkerStateModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------
+
+namespace Vasont.Inspire.Models.Worker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using Vasont.Inspire.Core.Errors;
+    using Vasont.Inspire.Core.Properties;
+
+    /// <summary>
+    /// Contains an enumerated list of worker process statuses.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum WorkerStatus
+    {
+        /// <summary>
+        /// The default status
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// The worker is working.
+        /// </summary>
+        Working,
+
+        /// <summary>
+        /// The worker is complete.
+        /// </summary>
+        Complete,
+
+        /// <summary>
+        /// The worker has failed.
+        /// </summary>
+        Failed
+    }
+
+    /// <summary>
+    /// This class represents a baseline worker state model.
+    /// </summary>
+    public class WorkerStateModel : MinimalWorkerStateModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerStateModel"/> class.
+        /// </summary>
+        public WorkerStateModel()
+        {
+            this.Status = WorkerStatus.Idle;
+            this.ProcessId = Guid.NewGuid();
+            this.Issues = new List<ErrorMessage>();
+            this.SubmittedUtcDate = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether an e-mail notification shall be sent to the submitting user once the import process is completed.
+        /// </summary>
+        public bool SendResultNotification { get; set; }
+
+        /// <summary>
+        /// Gets or sets the submitter user name.
+        /// </summary>
+        public string SubmittedByUserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the worker path for this process.
+        /// </summary>
+        public string WorkerPath { get; set; }
+
+        #region Public Methods
+        /// <summary>
+        /// This method is used to add an error message to the worker state model.
+        /// </summary>
+        /// <param name="errorMessage">Contains an instance of <see cref="ErrorMessage"/> class to add to the issues list.</param>
+        public void AddIssue(ErrorMessage errorMessage)
+        {
+            this.Issues.Add(errorMessage);
+        }
+
+        /// <summary>
+        /// This method is used to add an error message to the worker state model.
+        /// </summary>
+        /// <param name="messageKey">Contains the resource message key used for resource message lookup.</param>
+        /// <param name="type">Contains the optional error message type.</param>
+        /// <param name="parameters">Contains an optional array of parameters to add to the issue.</param>
+        public void AddIssue(string messageKey, ErrorType type = ErrorType.Fatal, params object[] parameters)
+        {
+            if (string.IsNullOrWhiteSpace(messageKey))
+            {
+                throw new ArgumentNullException(nameof(messageKey));
+            }
+
+            string message = Resources.ResourceManager.GetString(messageKey, Resources.Culture);
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                if (parameters != null)
+                {
+                    message = string.Format(CultureInfo.InvariantCulture, message, parameters);
+                }
+
+                this.AddIssue(new ErrorMessage(message, type));
+            }
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// This class represents worker state within the application that contains an additional 
+    /// model object.
+    /// </summary>
+    /// <typeparam name="T">Contains the object type of the additional worker model.</typeparam>
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Class files would be named the same.")]
+    public sealed class WorkerStateModel<T> : WorkerStateModel
+    {
+        #region Public Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerStateModel{T}"/> class.
+        /// </summary>
+        public WorkerStateModel()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerStateModel{T}"/> class.
+        /// </summary>
+        /// <param name="model">Contains any additional model used with the worker.</param>
+        public WorkerStateModel(T model)
+        {
+            this.Model = model;
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets or sets the optional model for the worker.
+        /// </summary>
+        public T Model { get; set; }
+    }
+}
