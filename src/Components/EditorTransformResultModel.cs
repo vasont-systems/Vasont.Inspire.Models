650e362ac2c37e3a93dd1e6973d06c85d08ff6b0
Rob Kennedy <rkennedy@vasont.com>
2020-02-06 15:41:53 -0500

    adding new editor related models

---
+++ src/Components/EditorTransformResultModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Vasont.Inspire.Models.Components
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class contains properties related to transforming content between html and xml.
+    /// </summary>
+    public class EditorTransformResultModel
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the content was successfully transformed.
+        /// </summary>
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content string to be transformed.
+        /// </summary>
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+    }
+}
---
+++ src/Components/EditorXmlErrorModel.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EditorXmlErrorModel.cs" company="GlobalLink Vasont">
+//     Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Components
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This class contains values used for an xml validation error.
+    /// </summary>
+    public class EditorXmlErrorModel
+    {
+        /// <summary>
+        /// Gets or sets the validation error message.
+        /// </summary>
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the validation type.
+        /// </summary>
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the error row number.
+        /// </summary>
+        [JsonProperty(PropertyName = "row")]
+        public long Row { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the error column number.
+        /// </summary>
+        [JsonProperty(PropertyName = "column")]
+        public long Column { get; set; }
+    }
+}
