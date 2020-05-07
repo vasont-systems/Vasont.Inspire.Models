9b25b46898735337057adf614a6907f5f997ce1c
Rob Kennedy <rkennedy@vasont.com>
2020-05-07 14:57:07 -0400

    updating models to support new display value pair model, new translation job state model, and localized descriptions for translation job states.

---
+++ src/Translations/TranslationJobStateModel.cs
@@ -0,0 +1,17 @@
+﻿//--------------------------------------------------------------------------
+// <copyright file="TranslationJobStateModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//--------------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Translations
+{
+    using System.Collections.Generic;
+    using Vasont.Inspire.Models.Common;
+
+    /// <summary>
+    /// This class represents a translation job state display value pair used for retrieving available states.
+    /// </summary>
+    public class TranslationJobStateModel : DisplayValuePair<string, string>
+    {
+    }
+}
