8816427c26b62f361ba4046c0263565d1b523ba8
Deanna Vezina <dvezina@vasont.com>
2020-08-25 10:17:54 -0400

    Restore models that were accidentally deleted for map sync of lock status

---
+++ src/Components/MapSyncRequestModel.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MapSyncRequestModel.cs" company="Talegen">
+//     Copyright (c) 2017 Talegen. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Inspire.WebApp.Api.Controllers.Content.Models
+{
+    /// <summary>
+    /// This class represents a model of the map sync request
+    /// </summary>
+    public class MapSyncRequestModel
+    {
+        /// <summary>
+        /// Gets or sets the node identity for the sync
+        /// </summary>
+        public string NodeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target component to return lock information.
+        /// </summary>
+        public long TargetComponentId { get; set; }
+    }
+}
---
+++ src/Components/MapSyncResponseModel.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MapSyncResponseModel.cs" company="Talegen">
+//     Copyright (c) 2017 Talegen. All rights reserved.
+// </copyright>
+// <author>Rob Kennedy</author>
+//-----------------------------------------------------------------------
+namespace Vasont.Inspire.WebApp.Api.Controllers.Content.Models
+{
+    using Vasont.Inspire.Models.Components;
+
+    /// <summary>
+    /// This class represents a response for the map sync request
+    /// </summary>
+    public class MapSyncResponseModel
+    {
+        /// <summary>
+        /// Gets or sets the node identity
+        /// </summary>
+        public string NodeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the lock information related to the target component.
+        /// </summary>
+        public ComponentLockModel Lock { get; set; }
+    }
+}
