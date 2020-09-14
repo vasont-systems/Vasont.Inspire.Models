e7bade174963f3e76ff17b5820d399d04a83bcc7
David Williamson <dwilliamson@vasont.com>
2020-09-14 08:59:33 -0400

    Recent Components

--- src/Components/Schema/ComponentUserAccessModel.cs
+++ src/Components/Schema/ComponentUserAccessModel.cs
@@ -3,7 +3,6 @@
 // Copyright (c) GlobalLink Vasont. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
-
 namespace Vasont.Inspire.Models.Components
 {
     using System;
@@ -15,22 +14,22 @@ namespace Vasont.Inspire.Models.Components
 	public class ComponentUserAccessModel
 	{
         /// <summary>
-        /// Gets or sets the associated project identity value.
+        /// Gets or sets the associated component identity value.
         /// </summary>
         public long ComponentId { get; set; }
 
         /// <summary>
-        /// Gets or sets the user identity of the user accessing the project.
+        /// Gets or sets the user identity of the user accessing the component.
         /// </summary>
         public long UserId { get; set; }
 
         /// <summary>
-        /// Gets or sets the date time when the project was last accessed.
+        /// Gets or sets the date time when the component was last accessed.
         /// </summary>
         public DateTime LastAccessedDate { get; set; }
 
         /// <summary>
-        /// Gets or sets the related project title.
+        /// Gets or sets the related component name.
         /// </summary>
         public string ComponentName { get; set; }
 
