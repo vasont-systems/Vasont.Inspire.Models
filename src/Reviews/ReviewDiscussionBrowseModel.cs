bdab0057ceaeeab5bfb25b8e24deaa1f0ee0d5c5
Deanna Vezina <dvezina@vasont.com>
2020-10-03 13:08:39 -0400

    New Branch for Review Discussion Models

---
+++ src/Reviews/ReviewDiscussionBrowseModel.cs
@@ -0,0 +1,46 @@
+﻿//--------------------------------------------------------------------
+// <copyright file="ReviewDiscussionBrowseModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//--------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Reviews
+{
+    using System;
+    using Vasont.Inspire.Models.Security;
+
+    /// <summary>
+    /// This class represents review discussions and review discussion replies within the application.
+    /// </summary>
+    public class ReviewDiscussionBrowseModel
+    {
+        /// <summary>
+        /// Gets or sets the identity of the review discussion.
+        /// </summary>
+        public long ReviewDiscussionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identity of the review.
+        /// </summary>
+        public long ReviewId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the review discussion user information.
+        /// </summary>
+        public MinimalUserModel User { get; set; } = new MinimalUserModel();
+
+        /// <summary>
+        /// Gets or sets the date and time when the review discussion was created.
+        /// </summary>
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Gets or sets the review discussion content.
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the review discussion replies.
+        /// </summary>
+        public ReviewDiscussionReplyModel ReviewDiscussionReply { get; set; } = new ReviewDiscussionReplyModel();
+    }
+}
---
+++ src/Reviews/ReviewDiscussionReplyModel.cs
@@ -0,0 +1,35 @@
+﻿//--------------------------------------------------------------------
+// <copyright file="ReviewDiscussionReplyModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//--------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Reviews
+{
+    using System;
+
+    /// <summary>
+    /// This class represents a review discussion  reply associated with a review discussion.
+    /// </summary>
+    public class ReviewDiscussionReplyModel
+    {
+        /// <summary>
+        /// Gets or sets the identity of the review discussion reply.
+        /// </summary>
+        public long ReviewDiscussionReplyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identity of the review discussion.
+        /// </summary>
+        public long ReviewDiscussionId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the review discussion reply was created.
+        /// </summary>
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// Gets or sets the review discussion reply content.
+        /// </summary>
+        public string Content { get; set; }
+    }
+}
