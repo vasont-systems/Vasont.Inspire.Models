3a2c6eb2744fa461e83718c87a86834c10ff05a6
David Williamson <dwilliamson@vasont.com>
2020-10-08 11:48:40 -0400

    componentpreviewtransformmodel

---
+++ src/Components/ComponentPreviewTransformModel.cs
@@ -0,0 +1,68 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ComponentPreviewTransformModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Components
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    /// <summary>
+    /// Contains an enumerated list of process types.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ComponentProcessType
+    {
+        /// <summary>
+        /// Contains a value that indicates the processtype is view.
+        /// </summary>
+        View,
+
+        /// <summary>
+        /// Contains a value that indicates the processtype is translation.
+        /// </summary>
+        Translation
+    }
+
+    /// <summary>
+    /// This class represents a component preview transform within the system and is to be used for interaction with application user interfaces.
+    /// </summary>
+    public class ComponentPreviewTransformModel
+    {
+        /// <summary>
+        /// Gets or sets the identity of the component preview transform.
+        /// </summary>
+        public long ComponentPreviewTransformsId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the component preview transform.
+        /// </summary>
+        public string ComponentPreviewTransformName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identity of the component type.
+        /// </summary>
+        public long ComponentTypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the review state information.
+        /// </summary>
+        public ComponentProcessType State { get; set; } = ComponentProcessType.Translation;
+
+        /// <summary>
+        /// Gets or sets the process type of the component preview transform.
+        /// </summary>
+        public string ProcessType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the XSLT of the component preview transform.
+        /// </summary>
+        public string XSLT { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CSS of the component preview transform.
+        /// </summary>
+        public string CSS { get; set; }
+    }
+}
