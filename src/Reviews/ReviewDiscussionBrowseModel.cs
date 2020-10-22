68f01864f3a5b8fd1646f039d2d6abc7aafbe374
Deanna Vezina <dvezina@vasont.com>
2020-10-22 11:05:25 -0400

    Browse Models for Review Discussion Reply

--- src/Reviews/ReviewDiscussionBrowseModel.cs
+++ src/Reviews/ReviewDiscussionBrowseModel.cs
@@ -42,6 +42,6 @@ namespace Vasont.Inspire.Models.Reviews
         /// <summary>
         /// Gets or sets the review discussion replies.
         /// </summary>
-        public List<ReviewDiscussionReplyModel> ReviewDiscussionReply { get; set; } = new List<ReviewDiscussionReplyModel>();
+        public List<ReviewDiscussionReplyBrowseModel> ReviewDiscussionReply { get; set; } = new List<ReviewDiscussionReplyBrowseModel>();
     }
 }
