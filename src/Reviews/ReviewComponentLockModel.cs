843624e4c38970f4e6560ef0c7d29ea5de98b83e
Deanna Vezina <dvezina@vasont.com>
2020-11-17 17:44:32 -0500

    Additional Models for New Review

---
+++ src/Reviews/ReviewComponentLockModel.cs
@@ -0,0 +1,39 @@
+﻿//-------------------------------------------------------------
+// <copyright file="ReviewComponentLockModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------
+namespace Vasont.Inspire.Models.ReviewComponents
+{
+    using System;
+    using Vasont.Inspire.Models.Security;
+
+    /// <summary>
+    /// This class represents a review component lock for a component within the application.
+    /// </summary>
+    public class ReviewComponentLockModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReviewComponentLockModel"/> class.
+        /// </summary>
+        public ReviewComponentLockModel()
+        {
+            this.LockedBy = new MicroUserModel();
+        }
+
+        /// <summary>
+        /// Gets or sets the review component identifier.
+        /// </summary>
+        public long ReviewComponentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the locked date information.
+        /// </summary>
+        public DateTime LockedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the locked by user model information.
+        /// </summary>
+        public MicroUserModel LockedBy { get; set; }
+    }
+}
---
+++ src/Reviews/ReviewComponentUnlockResultModel.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ReviewComponentUnlockResultModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Vasont.Inspire.Models.ReviewComponents
+{
+    /// <summary>
+    /// This class contains values used within the Unlocking of a review component.
+    /// </summary>
+    public class ReviewComponentUnlockResultModel
+    {
+        /// <summary>
+        /// Gets or sets the unique identity of the unlocking review component.
+        /// </summary>
+        public long ReviewComponentId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the unlock was complete.
+        /// </summary>
+        public bool Success { get; set; }
+    }
+}
