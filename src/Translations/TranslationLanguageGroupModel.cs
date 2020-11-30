90a2b3013f23bbcf868df2d0652b1a9571453ab2
Adam Magness <amagness@transperfect.com>
2020-11-30 14:23:45 -0500

    changes for TranslationLanguageGroups

---
+++ src/Translations/TranslationLanguageGroupModel.cs
@@ -0,0 +1,31 @@
+//--------------------------------------------------------------------------
+// <copyright file="TranslationLanguageGroupModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//--------------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Translations
+{
+    using System.Collections.Generic;
+    using Common;
+
+    /// <summary>
+    /// This class is a model representation of a minimal translation job within the application.
+    /// </summary>
+    public class TranslationLanguageGroupModel
+    {
+        /// <summary>
+        /// Gets or sets the identity of the translation language group.
+        /// </summary>
+        public long LanguageGroupId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the translation language group.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the translation language group languages.
+        /// </summary>
+        public List<MinimalLanguageModel> Languages { get; set; } = new List<MinimalLanguageModel>();
+    }
+}
