96b0d2db636d87b32b410ef43566ac96d5f3718d
Shaun Duffy <sduffy@vasont.com>
2021-04-14 12:18:42 -0400

    Added SubmissionAttributeModel

---
+++ src/Translations/SubmissionAttributeModel.cs
@@ -0,0 +1,37 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SubmissionAttributeModel.cs" company="Vasont Systems">
+//     Copyright (c) Vasont Systems. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Vasont.Inspire.Models.Translations
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// This class represents the optional submission attributes to send with a translation submission.
+    /// </summary>
+    public class SubmissionAttributeModel
+    {
+        /// <summary>Gets or sets the owner.</summary>
+        public string Owner { get; set; }
+
+        /// <summary>Gets or sets the submitter.</summary>
+        public string Submitter { get; set; }
+
+        /// <summary>Gets or sets the pm notes.</summary>
+        public string PmNotes { get; set; }
+
+        /// <summary>Gets or sets a value indicating whether this instance is urgent.</summary>
+        public bool IsUrgent { get; set; }
+
+        /// <summary>Gets or sets a comma separated list of the owners.</summary>
+        public string Owners { get; set; }
+
+        /// <summary>Gets or sets the meta data.</summary>
+        public Dictionary<string, string> MetaData { get; set; }
+
+        /// <summary>Gets or sets the custom attributes.</summary>
+        public Dictionary<string, string> CustomAttributes { get; set; }
+    }
+}
