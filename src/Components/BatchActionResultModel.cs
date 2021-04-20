29d9b15440fb929772a919b5ef1e2ddff7446fd2
Deanna Vezina <dvezina@vasont.com>
2021-04-20 15:32:23 -0400

    Add BatchActionResultModel for future use

---
+++ src/Components/BatchActionResultModel.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BatchActionResultModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Vasont.Inspire.Models.Components
+{
+    using System.Collections.Generic;
+    using Vasont.Inspire.Models.Common;
+
+    /// <summary>
+    /// This class contains properties and information related to a component API call that has failed.
+    /// </summary>
+    public class BatchActionResultModel : BatchActionResultModelBase
+    {
+        /// <summary>
+        /// Gets a list of <see cref="ComponentModel"/> objects that reference the component requested for action.
+        /// </summary>
+        public List<ComponentModel> SourceReferenceComponents { get; } = new List<ComponentModel>();
+    }
+}
