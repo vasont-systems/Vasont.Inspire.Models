260422afaa6290909b70c804373daf8840496bd3
Deanna Vezina <dvezina@vasont.com>
2021-09-20 15:20:58 -0400

    Changes for Review Report

---
+++ src/Reviews/ReviewReportStateModel.cs
@@ -0,0 +1,26 @@
+﻿//-------------------------------------------------------------
+// <copyright file="ReviewReportStateModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------
+namespace Vasont.Inspire.Models.Components
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// This class contains the state for review report request worker within the application.
+    /// </summary>
+    public class ReviewReportStateModel
+    {
+        /// <summary>
+        /// Gets or sets the process identity.
+        /// </summary>
+        public Guid ProcessId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of reviews to process.
+        /// </summary>
+        public List<long> ReviewIds { get; set; } = new List<long>();
+    }
+}
