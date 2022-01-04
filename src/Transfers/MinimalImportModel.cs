be7e5268e538b4ad9314970a2f3541567da66d2c
Deanna Vezina <dvezina@vasont.com>
2022-01-04 12:35:59 -0500

    commit more model changes for import and export transforms

---
+++ src/Transfers/MinimalImportModel.cs
@@ -0,0 +1,23 @@
+﻿//-------------------------------------------------------------
+// <copyright file="MinimalImportModel.cs" company="GlobalLink Vasont">
+// Copyright (c) GlobalLink Vasont. All rights reserved.
+// </copyright>
+//-------------------------------------------------------------
+namespace Vasont.Inspire.Models.Transfers
+{
+    /// <summary>
+    /// This class represents a minimal export model for use within the application.
+    /// </summary>
+    public class MinimalImportModel
+    {
+        /// <summary>
+        /// Gets or sets the unique identity of the import.
+        /// </summary>
+        public long ImportId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the import.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
